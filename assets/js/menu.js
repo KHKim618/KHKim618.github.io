79a075b750265c3d6fdb491ea00d595639ade442
Song Kim <songk@mit.edu>
2025-01-05 18:56:55 -0500

    menu transitions

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,4 +2,5 @@ function toggleDropdown() {
   document.getElementsByClassName("nav-links")[0].classList.toggle("show");
   document.getElementsByClassName("navicon")[0].classList.toggle("close");
   document.getElementsByClassName("menu-home")[0].classList.toggle("hide");
+  document.getElementsByClassName("menu-home-link")[0].classList.toggle("show");
 }
